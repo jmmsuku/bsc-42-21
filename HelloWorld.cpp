c048b7bf090485fbfadef196f80a57bd5c8a1123
jmmsuku <bsc-42-21@unima.ac.mw>
2024-05-13 14:33:41 -0600

    This is my second task in lab1 in c++

---
+++ HelloWorld.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <vector>
+#include <string>
+
+using namespace std;
+
+int main()
+{
+    vector<string> msg{"Hello", "c++", "world", "from", "vs code", "and the c++ extension!"};
+    for (const string & word : msg)
+    {
+        cout << word << " ";
+    }
+    cout << endl;
+}
